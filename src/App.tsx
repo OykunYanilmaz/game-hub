6d189cd4011ca5baf34658c85a930a38cb55e05f
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-03 22:06:26 +0300

    Fetch the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem } from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
+import GameGrid from "./components/GameGrid"
 
 function App() {
 
@@ -12,7 +13,9 @@ function App() {
         <NavBar></NavBar>
       </GridItem>
       <GridItem area='aside' bg='gold' display={{base: "none", lg: "block"}}>Aside</GridItem>
-      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+      <GridItem area='main'>
+        <GameGrid></GameGrid>
+      </GridItem>
     </Grid>
   )
 }
